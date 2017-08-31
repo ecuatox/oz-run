5561dbdd6cf59e1ed67880aa49631777ac5ed739
Amund Tenstad <github@amund.io>
2017-08-31 23:52:21 +0200

    Upload project

---
+++ spec/oz-run-spec.js
@@ -0,0 +1,18 @@
+'use babel';
+
+import OzRun from '../lib/oz-run';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('OzRun', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('oz-run');
+  });
+
+});
---
+++ spec/oz-run-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OzRunView from '../lib/oz-run-view';
+
+describe('OzRunView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
